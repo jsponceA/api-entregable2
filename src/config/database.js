cd19907015419b4dfb3e36ea676aece91fef2120
jsponceA <jonny47169@gmail.com>
2023-06-30 03:40:32 -0500

    env var

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("db_academlo_tarea_2", "postgres", "root", {
-  host: "localhost",
-  port: 5432,
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.BD_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 const validateConection = async () => {
   try {
